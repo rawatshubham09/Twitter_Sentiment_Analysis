72e94eb21782ca787bc245fd7d1045b21c0ae938
rawatshubham09 <rawatshubham09@gmail.com>
2024-12-28 15:23:12 +0530

    exception file created

--- demo.py
+++ demo.py
@@ -1,3 +1,13 @@
 from twitter_sentiment.logger import logging
+import sys
+
+from twitter_sentiment.exception import TwetterException
 
 logging.info("welcome in custome logger")
+
+
+try:
+    a=2/0
+
+except Exception as e:
+    raise TwetterException(e,sys)
