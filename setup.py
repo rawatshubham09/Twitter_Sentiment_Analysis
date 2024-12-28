2661974ea29598ca3c688643f5dca45a3589897f
rawatshubham09 <rawatshubham09@gmail.com>
2024-12-28 12:12:53 +0530

    "Requirements.txt updated, Setup file created"

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="twitter_sentiment",
+    version="0.0.0",
+    author="Shubham Rawat, Mukesh Kumar Singh",
+    author_email="rawatshubham09@gmail.com, mksingh1311@outlook.com",
+    packages=find_packages()
+)
