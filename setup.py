960896d602afcc94368eb99d7579a02ab297c793
rawatshubham09 <rawatshubham09@gmail.com>
2024-12-28 19:30:51 +0530

    setup update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="twitter_sentiment",
     version="0.0.0",
-    author="Shubham Rawat, Mukesh Kumar Singh",
-    author_email="rawatshubham09@gmail.com, mksingh1311@outlook.com",
+    author="Shubham Rawat",
+    author_email="rawatshubham09@gmail.com",
     packages=find_packages()
 )
